4fbcc3a5c4e262fcbc0ff9a370946bfcae6a7cee
arabkhondev <starc9899@gmail.com>
2025-04-02 16:47:10 +0500

    spatie install

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        $user = Auth::user();
+
+        if (!$user || !$user->hasRole($role)) {
+            abort(403, 'Bu sahifaga kirish taqiqlangan!');
+        }
+
+        return $next($request);
+    }
+}
+
